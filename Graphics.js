a717dcff944e6975b8bed7e5ad73008a67bc4d06
Roonerelli <chris.catherall@hotmail.com>
2013-08-14 21:13:46 +0100

    full screen on mobile

--- Graphics.js
+++ Graphics.js
@@ -613,6 +613,35 @@ var Game;
     Game.Tetris = Tetris;
 })(Game || (Game = {}));
 
+var page = document.getElementById('page'), ua = navigator.userAgent, iphone = ~ua.indexOf('iPhone') || ~ua.indexOf('iPod'), ipad = ~ua.indexOf('iPad'), ios = iphone || ipad, fullscreen = window.navigator.standalone, android = ~ua.indexOf('Android'), lastWidth = 0;
+
+if (android) {
+    window.onscroll = function () {
+        page.style.height = window.innerHeight + 'px';
+    };
+}
+var setupScroll = window.onload = function () {
+    if (ios) {
+        var height = document.documentElement.clientHeight;
+
+        if (iphone && !fullscreen)
+            height += 60;
+        page.style.height = height + 'px';
+    } else if (android) {
+        page.style.height = (window.innerHeight + 56) + 'px';
+    }
+
+    setTimeout(scrollTo, 0, 0, 1);
+};
+(window.onresize = function () {
+    var pageWidth = page.offsetWidth;
+
+    if (lastWidth == pageWidth)
+        return;
+    lastWidth = pageWidth;
+    setupScroll();
+})();
+
 var screenHeight = document.documentElement.clientHeight;
 var blockSize;
 
